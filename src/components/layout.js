3d30a8c19f45261e61f32de11a07673641ae4a49
DavidSilveraGabriel <ingenieria.d.s.g@hotmail.com>
2021-05-18 16:33:07 +0200

    test

--- src/components/layout.js
+++ src/components/layout.js
@@ -19,7 +19,7 @@ export default ({ children }) => {
     <div className="site-wrapper">
       <header className="site-header">
         <div className="site-title">
-          <a aria-current="page" href="https://devidblog.netlify.app/"><img href="../images/D_de.png" /></a>
+          <a aria-current="page" href="https://devidblog.netlify.app/"><img src="../icons/icon-48x48.png" alt="logo"/></a>
         </div>
         <Navigation />
       </header>
